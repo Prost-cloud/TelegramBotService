f0545aa7deb25075724380b332c0c5aa886d97b2
влад <vvladik9798@gmail.com>
2022-02-02 21:28:44 +0300

    Added DBProvider and other methods with adding fields with message info

--- TelegramBotService/CommandParcer/MethodProvider.cs
+++ TelegramBotService/CommandParcer/MethodProvider.cs
@@ -12,23 +12,23 @@ namespace CommandParcer
         public MethodProvider(IDbProvider dbProvider)
         {
             _dBProvider = dbProvider;
-            
+
             _dictionaryMethod = new Dictionary<string, Delegate>();
 
-            _dictionaryMethod.Add("/add",                    new Func<string, string, string, string, string> (_dBProvider.AddProduct));
-            _dictionaryMethod.Add("/addupdate",              new Func<string, string, string, string, string>(_dBProvider.AddUpdate));
-            _dictionaryMethod.Add("/delete",                 new Func<string, string, string, string>         (_dBProvider.DeleteProduct));
-            _dictionaryMethod.Add("/addpayer",               new Func<string, string, string, string>         (_dBProvider.AddPayer));
-            _dictionaryMethod.Add("/deletepayer",            new Func<string, string, string, string>         (_dBProvider.DeletePayer));
-            _dictionaryMethod.Add("/addfunds",               new Func<string, string, string, string, string> (_dBProvider.AddFunds));
-            _dictionaryMethod.Add("/removefunds",            new Func<string, string, string, string, string> (_dBProvider.RemoveFunds));
-            _dictionaryMethod.Add("/create",                 new Func<string, string, string, string>         (_dBProvider.AddShoppingList));
-            _dictionaryMethod.Add("/deleteshoppinglist",     new Func<string, string, string, string>         (_dBProvider.DeleteShoppingList));
-            _dictionaryMethod.Add("/select",                 new Func<string, string, string, string>         (_dBProvider.SelectShoppingList));
-            _dictionaryMethod.Add("/count",                  new Func<string, string, string>                 (_dBProvider.GetCountingByPayers));
-            _dictionaryMethod.Add("/info",                   new Func<string, string, string>                 (_dBProvider.GetShoppingLists));
-            _dictionaryMethod.Add("/show",                   new Func<string, string, string, string>         (_dBProvider.Show));
-            _dictionaryMethod.Add("/start",                  new Func<string, string, string>                 (_dBProvider.AddUser));
+            _dictionaryMethod.Add("/add",                    new Func<string, string, string> (_dBProvider.AddProduct));
+            _dictionaryMethod.Add("/addupdate",              new Func<string, string, string> (_dBProvider.UpdateProduct));
+            _dictionaryMethod.Add("/delete",                 new Func<string, string>         (_dBProvider.DeleteProduct));
+            _dictionaryMethod.Add("/addpayer",               new Func<string, string>         (_dBProvider.AddPayer));
+            _dictionaryMethod.Add("/deletepayer",            new Func<string, string>         (_dBProvider.DeletePayer));
+            _dictionaryMethod.Add("/addfunds",               new Func<string, string, string> (_dBProvider.AddFunds));
+            _dictionaryMethod.Add("/removefunds",            new Func<string, string, string> (_dBProvider.RemoveFunds));
+            _dictionaryMethod.Add("/create",                 new Func<string, string>         (_dBProvider.AddShoppingList));
+            _dictionaryMethod.Add("/deleteshoppinglist",     new Func<string, string>         (_dBProvider.DeleteShoppingList));
+            _dictionaryMethod.Add("/select",                 new Func<string, string>         (_dBProvider.SelectShoppingList));
+            _dictionaryMethod.Add("/count",                  new Func<string>                 (_dBProvider.GetCountingByPayers));
+            _dictionaryMethod.Add("/info",                   new Func<string>                 (_dBProvider.GetShoppingLists));
+            _dictionaryMethod.Add("/show",                   new Func<string, string>         (_dBProvider.Show));
+            _dictionaryMethod.Add("/start",                  new Func<string>                 (_dBProvider.Start));
         }
 
         public string Invoke(string method, params string[] args)
